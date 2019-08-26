ee56b68033518ca9650cbbcdcfc0c773d2e4d0fb
Roman Koldaev <harp07@mail.ru>
2019-08-26 13:01:08 +0400

    ip + mask of interfaces

--- src/main/java/my/harp07/SnmpWalk.java
+++ src/main/java/my/harp07/SnmpWalk.java
@@ -5,7 +5,9 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
+import java.util.stream.Collectors;
 import static java.util.stream.Collectors.toList;
+import java.util.stream.Stream;
 import org.apache.commons.lang3.StringUtils;
 import org.snmp4j.CommunityTarget;
 import org.snmp4j.Snmp;
@@ -22,14 +24,14 @@ import org.snmp4j.util.TreeEvent;
 import org.snmp4j.util.TreeUtils;
 
 public class SnmpWalk {
-    
-    static String IP="10.73.2.22";
+
+    static String IP = "10.73.2.22";
     static Map<String, String> walkMap;
-    static Map<String, Integer> versionMap=new HashMap<>();
-    static String snmp_comm="look";
-    static String snmp_vers="2";//SnmpConstants.version2c;
-    static String snmp_port="161";
-    
+    static Map<String, Integer> versionMap = new HashMap<>();
+    static String snmp_comm = "look";
+    static String snmp_vers = "2";//SnmpConstants.version2c;
+    static String snmp_port = "161";
+
     // standart mibs = 
     // IP-MIB, 
     // IP-FORWARD-MIB, 
@@ -39,50 +41,75 @@ public class SnmpWalk {
     // UDP-MIB, 
     // EtherLike-MIB
     // SNMPv2-MIB,
-    final static String       base = "1.3.6.1.2.1.";
+    final static String base = "1.3.6.1.2.1.";
     final static String system_mib = "1";  // SNMPv2-MIB,
-    final static String     if_mib = "2";  // IF-MIB,
-    final static String     at_mib = "3";  // address translation
-    final static String     ip_mib = "4";  // IP-MIB, 
-    final static String   icmp_mib = "5"; 
-    final static String    tcp_mib = "6";  // TCP-MIB, 
-    final static String    udp_mib = "7";  // UDP-MIB,    
-    final static String    dot_mib = "10"; // = transmission = EtherLike-MIB
-    final static String   snmp_mib = "11"; // SNMPv2-MIB,
-    final static String    ifX_mib = "31";
-    
+    final static String if_mib = "2";  // IF-MIB,
+    final static String at_mib = "3";  // address translation
+    final static String ip_mib = "4";  // IP-MIB, 
+    final static String icmp_mib = "5";
+    final static String tcp_mib = "6";  // TCP-MIB, 
+    final static String udp_mib = "7";  // UDP-MIB,    
+    final static String dot_mib = "10"; // = transmission = EtherLike-MIB
+    final static String snmp_mib = "11"; // SNMPv2-MIB,
+    final static String ifX_mib = "31";
+
     // ip-addresses of interfaces:
     final static String base_ipIF = base + ip_mib;
     final static String ip_index = ".20.1.2";
     final static String ip_masks = ".20.1.3";
-    
+
     static {
-       versionMap.put("1", SnmpConstants.version1);
-       versionMap.put("2", SnmpConstants.version2c);
-       versionMap.put("3", SnmpConstants.version3);
+        versionMap.put("1", SnmpConstants.version1);
+        versionMap.put("2", SnmpConstants.version2c);
+        versionMap.put("3", SnmpConstants.version3);
     }
-    
 
     public static void main(String[] args) throws Exception {
         walkMap = walkSNMP(IP, base + ip_mib + ".20.1.3", snmp_comm, snmp_port, snmp_vers); // ifTable, mib-2 interfaces
-        walkMap.entrySet().forEach( x->{
-           System.out.println(x.getKey() + " = " + x.getValue()); 
+        walkMap.entrySet().forEach(x -> {
+            System.out.println(x.getKey() + " = " + x.getValue());
         });
-        List<String> listIP_INDEX = walkSNMP(IP, base_ipIF+ip_index, snmp_comm, snmp_port, snmp_vers)
-                .entrySet().stream().map(x->StringUtils.substringAfter(x.getKey(), base_ipIF+ip_index+".")+"="+x.getValue()).collect(toList());
-        List<String> listIP_MASKS = walkSNMP(IP, base_ipIF+ip_masks, snmp_comm, snmp_port, snmp_vers)
-                .entrySet().stream().map(x->StringUtils.substringAfter(x.getKey(), base_ipIF+ip_masks+".")+"="+x.getValue()).collect(toList());
-        System.out.println("\n listIP_INDEX = "+listIP_INDEX);
-        System.out.println("\n listIP_MASKS = "+listIP_MASKS);
+        List<String> listIP_INDEX = walkSNMP(IP, base_ipIF + ip_index, snmp_comm, snmp_port, snmp_vers)
+                .entrySet().stream().map(x -> StringUtils.substringAfter(x.getKey(), base_ipIF + ip_index + ".") + "=" + x.getValue()).collect(toList());
+        List<String> listIP_MASKS = walkSNMP(IP, base_ipIF + ip_masks, snmp_comm, snmp_port, snmp_vers)
+                .entrySet().stream().map(x -> StringUtils.substringAfter(x.getKey(), base_ipIF + ip_masks + ".") + "=" + x.getValue()).collect(toList());
+        System.out.println("\n listIP_INDEX = " + listIP_INDEX);
+        System.out.println("\n listIP_MASKS = " + listIP_MASKS);
+        Map<String, String> mapIP_INDEX = walkSNMP(IP, base_ipIF + ip_index, snmp_comm, snmp_port, snmp_vers)
+                .entrySet().stream().collect(Collectors.toMap(
+                        x -> StringUtils.substringAfter(x.getKey(), base_ipIF + ip_index + "."),
+                        x -> x.getValue()));
+        Map<String, String> mapIP_MASKS = walkSNMP(IP, base_ipIF + ip_masks, snmp_comm, snmp_port, snmp_vers)
+                .entrySet().stream().collect(Collectors.toMap(
+                        x -> StringUtils.substringAfter(x.getKey(), base_ipIF + ip_masks + "."),
+                        x -> x.getValue()));
+        System.out.println("\n mapIP_INDEX = " + mapIP_INDEX);
+        System.out.println("\n mapIP_MASKS = " + mapIP_MASKS);        
+        Stream combined = Stream.concat(mapIP_INDEX.entrySet().stream(), mapIP_MASKS.entrySet().stream());
+        Map<String, String> result=new HashMap<>();
+        /*for (Map.Entry<String, String> ipi : mapIP_INDEX.entrySet()) {
+            for (Map.Entry<String, String> ipm : mapIP_MASKS.entrySet()) {
+                if (ipi.getKey().equals(ipm.getKey())) {
+                    result.put(ipi.getValue(), ipi.getKey() + " " + ipm.getValue());
+                }
+ 
+            }
+        } */       
+        mapIP_INDEX.entrySet().forEach((ipi) -> {
+            mapIP_MASKS.entrySet().stream().filter((ipm) -> (ipi.getKey().equals(ipm.getKey()))).forEachOrdered((ipm) -> {
+                result.put(ipi.getValue(), ipi.getKey() + " " + ipm.getValue());
+            });
+        });
+        System.out.println("\n result = " + result);
     }
 
     public static Map<String, String> walkSNMP(String ip, String oid, String comm, String port, String vers) throws IOException {
         CommunityTarget target = new CommunityTarget();
         target.setCommunity(new OctetString(comm));
-        target.setAddress(GenericAddress.parse("udp:"+ip+"/"+port)); // supply your own IP and snmp_port
+        target.setAddress(GenericAddress.parse("udp:" + ip + "/" + port)); // supply your own IP and snmp_port
         target.setRetries(2);
         target.setTimeout(1000);
-        target.setVersion(versionMap.get(vers));        
+        target.setVersion(versionMap.get(vers));
         Map<String, String> result = new TreeMap<>();
         TransportMapping<? extends Address> transport = new DefaultUdpTransportMapping();
         Snmp snmp = new Snmp(transport);
